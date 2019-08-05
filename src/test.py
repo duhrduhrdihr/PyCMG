40de33897854239f5be8bd94599cc47eb9c33f7f
rodrigoveraa <rodrigoeveraa@gmail.com>
2019-08-05 11:57:35 -0400

    Removed unnecessary comments

--- src/test.py
+++ src/test.py
@@ -1,15 +1,8 @@
-import winsound
 import random
 import math
 from pysine import sine
-# from threading import Thread
 import multiprocessing
 
-# winsound.Beep(freq, duration)
-# freq: Hz, 37-32767
-# duration: milliseconds
-
-#winsound.Beep(440, 2000)
 
 f0 = 440.0
 a = math.pow(2, 1/12)
@@ -22,12 +15,8 @@ MIN_NOTE_LENGTH = 100
 MAX_NOTE_LENGTH = 1000
 NOTE_LENGTH_STEP = 50
 
-# for n in notes:
-#     print(n)
-
 
 class Note:
-    """docstring for ."""
 
     def __init__(self, frequency, duration):
         self.frequency = frequency
@@ -35,7 +24,6 @@ class Note:
 
     def play(self):
         sine(frequency=self.frequency, duration=self.duration)
-        # print("N: {}".format(self.duration))
 
 
 class Phrase:
@@ -49,7 +37,6 @@ class Phrase:
         self.note_set = NOTES[self.seed-3:self.seed+4]
 
     def is_full(self):
-        # return len(self.notes) >= self.length
         return self.time_remaining() <= 0
 
     def add_note(self, frequency, duration):
@@ -73,8 +60,6 @@ class Phrase:
             total += n.duration
             print("TOTAL: {}".format(total))
         print("END OF PHRASE")
-        # print(NOTES[self.seed])
-        # print(self.note_set)
 
     def time_remaining(self):
         return self.max_time_length - self.time_length
@@ -92,33 +77,21 @@ def play_voice():
     phrases = []
     for i in range(NUMBER_OF_PHRASES):
 
-        p = Phrase(random.choice(PHRASE_LENGTH), 5.0)
-        # freq = random.randrange(100, 1000, 1)
-        while not p.is_full():
+        phrase = Phrase(random.choice(PHRASE_LENGTH), 5.0)
+        while not phrase.is_full():
 
-            # freq = random.choice(NOTES)
-            freq = p.pick_note()
+            freq = phrase.pick_note()
             dur = random.randrange(MIN_NOTE_LENGTH, MAX_NOTE_LENGTH, NOTE_LENGTH_STEP)/1000.0
-            p.add_note(freq, dur)
-            # sine(frequency=freq, duration=dur)
-        phrases.append(p)
-    #    winsound.Beep(freq, dur)
+            phrase.add_note(freq, dur)
+        phrases.append(phrase)
 
-    for p in phrases:
-        p.play()
+    for phrase in phrases:
+        phrase.play()
 
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
-    # multiprocessing.set_start_method('spawn')
     p = multiprocessing.Process(target=play_voice)
     p.start()
     play_voice()
     p.join()
-
-# t1 = Thread(target=play_voice)
-# t2 = Thread(target=play_voice)
-# t1.start()
-# t2.start()
-
-#sine(frequency=440.0, duration=2.0)
